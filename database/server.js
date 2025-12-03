e97fcc5429b62b83fdeb279cc9cac2b9edb1d7de
Dhanush G Shetty <dhanushgshetty666@gmail.com>
2025-12-03 15:26:57 +0530

    Enable SSL and custom port for Cloud Database connection

--- database/server.js
+++ database/server.js
@@ -36,6 +36,8 @@ if (process.env.DB_HOST) {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306,
+    ssl: process.env.DB_HOST === 'localhost' ? undefined : { rejectUnauthorized: false },
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
